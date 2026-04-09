b15667eec90cbf9a0d102c919ea446a296b1c4a6
yuuki <aditya.garimella2002@gmail.com>
2026-04-10 07:41:11 +0900

    Fixed Optional fields bug

--- backend/internal/domain/Link.go
+++ backend/internal/domain/Link.go
@@ -6,7 +6,7 @@ import "time"
 type LinkDB struct{
 	BundleSlug string `db:"bundle_slug"`
 	Url string `db:"url"`
-	Note string `db:"note"`
-	DisplayText string `db:"display_text"`
+	Note *string `db:"note"`
+	DisplayText *string `db:"display_text"`
 	CreatedAt time.Time `db:"created_at"`
 } 
